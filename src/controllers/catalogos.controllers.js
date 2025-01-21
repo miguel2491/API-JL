5c8effbedfebaf308de5fd32f90a25245b2b9ad8
Miguelin Ds <miguelin-2491@hotmail.com>
2025-01-20 21:50:31 -0600

    APIS
    Principales

--- src/controllers/catalogos.controllers.js
+++ src/controllers/catalogos.controllers.js
@@ -205,10 +205,213 @@ export const delPermiso = async (req, res) => {
         return res.status(500).json({ mssg: 'Error al procesar la solicitud' });
     }
 };
-
-//====================== CRUD PRODUCTO ===============================
-
 //====================== CRUD CATEGORIA ===============================
+export const setCategoria = async (req, res) =>{
+    const pool = await getConnection()
+    const result = await pool.request()
+    .input('nombre', sql.VarChar, req.body.nombre)
+    .input('estatus', sql.NChar, '1')
+    .query('INSERT INTO tbc_Categoria(nombre, estatus)'+
+        ' VALUES (@nombre, @estatus);'+
+        ' SELECT SCOPE_IDENTITY() AS id;')
+    res.json({
+        id:result.recordset[0].id,
+        message:'Se agrego Correctamente'
+    })
+}
+export const getCategorias = async (req, res) =>{
+    const pool = await getConnection()    
+    const result = await pool.request()
+    .query('SELECT * FROM tbc_Categoria')
+    if(result.rowsAffected[0] === 0){
+        return res.status(404).json({message:"No encontrado"});
+    }
+    const trimmedRecords = result.recordset.map(record => {
+        // Iterar sobre las propiedades del registro y aplicar trim()
+        const trimmedRecord = {};
+        for (let key in record) {
+            if (record.hasOwnProperty(key) && typeof record[key] === 'string') {
+                trimmedRecord[key] = record[key].trim();  // Aplicar trim a las cadenas de texto
+            } else {
+                trimmedRecord[key] = record[key];  // Mantener los demás campos tal como están
+            }
+        }
+        return trimmedRecord;
+    });
+    res.json(trimmedRecords)
+};
+export const getCategoria = async (req, res) => {
+    const { id } = req.params;  // Usar req.params para obtener el id de la URL
 
+    // Verificar que el id esté presente y sea un número entero
+    if (!id || isNaN(id)) {
+        return res.status(400).json({ message: 'ID inválido o no proporcionado' });
+    }
+
+    try {
+        const pool = await getConnection();  // Obtener conexión a la base de datos
+        const result = await pool.request()
+            .input('id', sql.Int, id)  // Pasar el id como parámetro
+            .query("SELECT * FROM tbc_Categoria WHERE id = @id");
+
+        // Verificar si se encontraron registros
+        if (result.recordset.length === 0) {
+            return res.status(404).json({ message: 'No encontrado' });
+        }
+
+        // Aplicar trim() a los valores de los campos de cada registro
+        const trimmedRecords = result.recordset.map(record => {
+            const trimmedRecord = {};
+            for (let key in record) {
+                if (record.hasOwnProperty(key) && typeof record[key] === 'string') {
+                    trimmedRecord[key] = record[key].trim();  // Aplicar trim a las cadenas de texto
+                } else {
+                    trimmedRecord[key] = record[key];  // Mantener los demás campos tal como están
+                }
+            }
+            return trimmedRecord;
+        });
+
+        // Devolver los registros "trimmeados"
+        res.json(trimmedRecords);
+
+    } catch (error) {
+        console.error('Error al obtener el rol:', error);
+        return res.status(500).json({ message: 'Error al procesar la solicitud' });
+    }
+};
+export const delCategoria = async (req, res) => {
+    const { Id } = req.body;  // Desestructuración del id desde el cuerpo de la solicitud
+    
+    if (!Id) {
+        return res.status(400).json({ mssg: 'El campo "Id" es obligatorio' });
+    }
+
+    try {
+        const pool = await getConnection();  // Obtener conexión a la base de datos
+        const result = await pool.request()
+            .input('id', sql.Int, Id)  // Pasar el Id como parámetro
+            .query("UPDATE tbc_Categoria SET estatus = '0' WHERE id = @id");  // Actualizar el estatus del permiso
+
+        // Verificar si se actualizó algún registro
+        if (result.rowsAffected[0] === 0) {
+            return res.status(404).json({ mssg: 'No se encontró el permiso con el id proporcionado' });
+        }
+
+        // Responder con un mensaje de éxito
+        res.json({
+            mssg: 'Permiso eliminado correctamente',
+            id: Id  // Retornar el id del permiso actualizado
+        });
+    } catch (error) {
+        console.error('Error al actualizar permisos:', error);
+        return res.status(500).json({ mssg: 'Error al procesar la solicitud' });
+    }
+};
 //====================== CRUD PROVEEDOR ===============================
+export const setProveedor = async (req, res) =>{
+    const pool = await getConnection()
+    const result = await pool.request()
+    .input('id_categoria', sql.Int, req.body.id_categoria)
+    .input('nombre', sql.VarChar, req.body.nombre)
+    .input('ubicacion', sql.VarChar, req.body.ubicacion)
+    .input('estatus', sql.NChar, '1')
+    .query('INSERT INTO tbc_Proveedor(id_categoria,nombre, ubicacion,estatus)'+
+        ' VALUES (@id_categoria, @nombre, @ubicacion, @estatus);'+
+        ' SELECT SCOPE_IDENTITY() AS id;')
+    res.json({
+        id:result.recordset[0].id,
+        message:'Se agrego Correctamente'
+    })
+}
+export const getProveedores = async (req, res) =>{
+    const pool = await getConnection()    
+    const result = await pool.request()
+    .query('SELECT * FROM tbc_Proveedor')
+    if(result.rowsAffected[0] === 0){
+        return res.status(404).json({message:"No encontrado"});
+    }
+    const trimmedRecords = result.recordset.map(record => {
+        // Iterar sobre las propiedades del registro y aplicar trim()
+        const trimmedRecord = {};
+        for (let key in record) {
+            if (record.hasOwnProperty(key) && typeof record[key] === 'string') {
+                trimmedRecord[key] = record[key].trim();  // Aplicar trim a las cadenas de texto
+            } else {
+                trimmedRecord[key] = record[key];  // Mantener los demás campos tal como están
+            }
+        }
+        return trimmedRecord;
+    });
+    res.json(trimmedRecords)
+};
+export const getProveedor = async (req, res) => {
+    const { id } = req.params;  // Usar req.params para obtener el id de la URL
+
+    // Verificar que el id esté presente y sea un número entero
+    if (!id || isNaN(id)) {
+        return res.status(400).json({ message: 'ID inválido o no proporcionado' });
+    }
+
+    try {
+        const pool = await getConnection();  // Obtener conexión a la base de datos
+        const result = await pool.request()
+            .input('id', sql.Int, id)  // Pasar el id como parámetro
+            .query("SELECT * FROM tbc_Proveedor WHERE id = @id");
+
+        // Verificar si se encontraron registros
+        if (result.recordset.length === 0) {
+            return res.status(404).json({ message: 'No encontrado' });
+        }
+
+        // Aplicar trim() a los valores de los campos de cada registro
+        const trimmedRecords = result.recordset.map(record => {
+            const trimmedRecord = {};
+            for (let key in record) {
+                if (record.hasOwnProperty(key) && typeof record[key] === 'string') {
+                    trimmedRecord[key] = record[key].trim();  // Aplicar trim a las cadenas de texto
+                } else {
+                    trimmedRecord[key] = record[key];  // Mantener los demás campos tal como están
+                }
+            }
+            return trimmedRecord;
+        });
+
+        // Devolver los registros "trimmeados"
+        res.json(trimmedRecords);
+
+    } catch (error) {
+        console.error('Error al obtener el rol:', error);
+        return res.status(500).json({ message: 'Error al procesar la solicitud' });
+    }
+};
+export const delProveedor = async (req, res) => {
+    const { Id } = req.body;  // Desestructuración del id desde el cuerpo de la solicitud
+    
+    if (!Id) {
+        return res.status(400).json({ mssg: 'El campo "Id" es obligatorio' });
+    }
+
+    try {
+        const pool = await getConnection();  // Obtener conexión a la base de datos
+        const result = await pool.request()
+            .input('id', sql.Int, Id)  // Pasar el Id como parámetro
+            .query("UPDATE tbc_Proveedor SET estatus = '0' WHERE id = @id");  // Actualizar el estatus del permiso
+
+        // Verificar si se actualizó algún registro
+        if (result.rowsAffected[0] === 0) {
+            return res.status(404).json({ mssg: 'No se encontró el permiso con el id proporcionado' });
+        }
+
+        // Responder con un mensaje de éxito
+        res.json({
+            mssg: 'Permiso eliminado correctamente',
+            id: Id  // Retornar el id del permiso actualizado
+        });
+    } catch (error) {
+        console.error('Error al actualizar permisos:', error);
+        return res.status(500).json({ mssg: 'Error al procesar la solicitud' });
+    }
+};
+//====================== CRUD PRODUCTO ===============================
 
--- src/routes/catalogos.routes.js
+++ src/routes/catalogos.routes.js
@@ -1,13 +1,27 @@
 import { Router } from "express";
-import { setRoles, setPermiso, getRoles, getPermisos, getRolId, getPermisoId, delRol, delPermiso } from '../controllers/catalogos.controllers.js'
+import { setRoles, getRoles,getRolId, delRol,
+    getPermisos,  setPermiso,getPermisoId, delPermiso, 
+setCategoria, getCategorias, getCategoria, delCategoria,
+setProveedor, getProveedores, getProveedor, delProveedor 
+} from '../controllers/catalogos.controllers.js'
 
 const router = Router()
-
+//CATEGORIAS
+router.get('/categorias', getCategorias);
+router.get('/categoria/:id', getCategoria);
+router.post('/categoria', setCategoria);
+router.delete('/categoria/:id', delCategoria);
+//PROVEEDORES
+router.get('/proveedores', getProveedores);
+router.get('/proveedor/:id', getProveedor);
+router.post('/proveedor', setProveedor);
+router.delete('/proveedor/:id', delProveedor);
+//ROLES
 router.get('/roles', getRoles);
 router.get('/rol/:id', getRolId);
 router.post('/rol', setRoles);
 router.delete('/rol/:id', delRol);
-
+//PERMISOS
 router.get('/permisos', getPermisos);
 router.post('/permiso', setPermiso)
 router.get('/permisos/:id', getPermisoId);
